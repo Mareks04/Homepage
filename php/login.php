de16c734fdb957949abcc2ebbffc011df251b6fd
Mareks <56604457+Mareks04@users.noreply.github.com>
2021-02-04 15:35:44 +0200

    added index.html

--- php/login.php
+++ php/login.php
@@ -5,7 +5,7 @@
 $mysqli = new mysqli('localhost', 'root', '', 'cl_database');
 $result = $mysqli->query("SELECT id FROM clients_db WHERE fldEmail = '$user'");
 if (!empty($result) && $result->num_rows > 0) {
-    header("Location: ../html/home.html");
+    header("Location: ../html/index.html");
     exit();
 } else {
     echo "Please register first!";
