0604169c0849037f125a3c6670425523a6a1406c
Mareks <56604457+Mareks04@users.noreply.github.com>
2021-02-24 00:10:09 +0200

    Small updates

--- js/home.js
+++ js/home.js
@@ -10,40 +10,55 @@ toggleButton.addEventListener('click', () => {
 
 // Inventory start
 function openModal() {
-  document.getElementById("myModal").style.display = "block";
+    if (screen.width > 450) {
+        document.getElementById("myModal").style.display = "block";
+    }
 }
 
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+    document.getElementById("myModal").style.display = "none";
 }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+
+var lightboxOnResize = function lightboxOnResize() {
+    if ($(window).width() < 450) {
+        $('a[rel="lightbox"]')
+            .removeProp('rel')
+            .addClass('lightboxRemoved');
+    } else {
+        $('a.lightboxRemoved').prop('rel', 'lightbox');
+    }
+}
+
+$(document).ready(lightboxOnResize);
+$(window).resize(lightboxOnResize);
 
 // Inventory end
